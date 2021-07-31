dfc75675b0872183f6412fda87d9f686832a4171
affyzal <32396442+affyzal@users.noreply.github.com>
2021-07-31 01:33:39 +0100

    initialise router, homepage and created homebar component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Homepage from './homepage';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <BrowserRouter>
+        <Switch>
+            <Route exact path="/" render={(props) => <Homepage {...props} />} />
 
+        </Switch>
+      </BrowserRouter>
+  )     ;
+};
 export default App;
---
+++ src/homebar.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import 'fontsource-roboto';
+import {Grid} from "@material-ui/core";
+import Content from "./Content";
+import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
+import ThreeDRotation from '@material-ui/icons/ThreeDRotation';
+import HomeIcon from '@material-ui/icons/Home';
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import Switch from '@material-ui/core/Switch';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+
+export default function ButtonAppBar() {
+    const [auth, setAuth] = React.useState(true);
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+
+    const handleChange = (event) => {
+        setAuth(event.target.checked);
+    };
+
+    const handleMenu = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+    return (
+        <div>
+            <AppBar position="static" color="primary">
+                <Toolbar>
+                    <div>
+                        <Link to="/">
+                            <HomeIcon fontSize="large" />
+                        </Link>
+                    </div>
+                    <Grid
+                        container
+                        direction="column"
+                        justify="space-between"
+                        alignItems="flex-start">
+                        <Typography variant="h6" >
+                            Trubshaw Cumberledge Coding Test
+                        </Typography>
+                    </Grid>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
+}
