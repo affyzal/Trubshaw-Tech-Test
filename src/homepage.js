1a8e6e8211efe4f9b8087873306fd63a94937187
affyzal <32396442+affyzal@users.noreply.github.com>
2021-07-31 16:47:52 +0100

    Homepage UX implemented

--- src/homepage.js
+++ src/homepage.js
@@ -1,27 +1,35 @@
 import React, { useState } from "react";
-import logo from "./logo.svg";
+import logo from "./logo.png";
 import HomeBar from "./homebar"
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Typography from "@material-ui/core/Typography";
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+import Fab from '@material-ui/core/Fab';
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
+import Zoom from '@material-ui/core/Zoom';
 
-const homepage = () => {
+function homepage(props) {
+
     return (
-
-        <div className="App">
+        <div>
             <HomeBar />
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React Programming
-                </a>
+                <form noValidate autoComplete="off">
+                    <TextField id="outlined-basic" label="What is your name?(2-25 characters)" variant="outlined" />
+                </form>
+                <Typography display="block"> My text will be on the next line </Typography>
+                <Button variant="contained" color="primary" onClick={() => { alert('clicked')}}>
+                    Next
+                </Button>
             </header>
-        </div>
+</div>
+
     )
 }
 
